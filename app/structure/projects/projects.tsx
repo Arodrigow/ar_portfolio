8a931326344695d5a0cd89bda971859dfa334e3f
Arodrigow <rodrigow_@hotmail.com.br>
2025-05-13 14:17:17 -0300

    Fixed more temporary styling on navbar

---
+++ app/structure/projects/projects.tsx
@@ -0,0 +1,14 @@
+const ProjectsSection = () => {
+    return (
+        <div id="projects" className="
+                 flex flex-row justify-center items-center 
+                 h-[calc(100vh-48px)] w-full 
+                 px-40 gap-20
+                 bg-gradient-to-b from-[#0a192f] to-[#0a192f]
+                 "
+        >
+        </div>
+    )
+}
+
+export default ProjectsSection;

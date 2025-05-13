1eafe27020767ba0ba6bd52380f4f2e9de7711a8
Arodrigow <rodrigow_@hotmail.com.br>
2025-05-13 14:12:25 -0300

    Fixed navbar color

--- app/structure/navbar/navbar.tsx
+++ app/structure/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import NavLogo from "./logo";
 const Navbar = () => {
     const topics = ['Início', 'Sobre', 'Projetos', 'Habilidades', 'Contato']
     const hrefs = ['home', 'about', 'projects', 'skills', 'contact']
-    return <nav className="flex flex-row justify-between items-center sticky top-0 h-[48px] border">
+    return <nav className="flex flex-row justify-between items-center sticky top-0 h-[48px] border bg-[#0a192f]">
         <NavLogo></NavLogo>
         <div className="flex flex-row gap-2 bg-[#0a192f]">
             {

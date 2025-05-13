64ac320b61cb76c9ba4c6fe0b0320bd342567576
Arodrigow <rodrigow_@hotmail.com.br>
2025-05-13 14:08:06 -0300

    Added href to my navbar

--- app/structure/navbar/button.tsx
+++ app/structure/navbar/button.tsx
@@ -1,10 +1,11 @@
 type props = {
     name: string;
+    href: string;
 }
 const NavButton = (props: props) => {
     return (
         <button className="bg-transparent hover:underline hover:text-shadow-[1px_0_0_rgb(0_0_0)] tracking-wider text-white font-semibold py-2 px-4 cursor-pointer">
-            {props.name}
+           <a href={"#"+props.href}>{props.name}</a>
         </button>
     )
 }
--- app/structure/navbar/navbar.tsx
+++ app/structure/navbar/navbar.tsx
@@ -3,13 +3,14 @@ import NavLogo from "./logo";
 
 const Navbar = () => {
     const topics = ['Início', 'Sobre', 'Projetos', 'Habilidades', 'Contato']
+    const hrefs = ['home', 'about', 'projects', 'skills', 'contact']
     return <nav className="flex flex-row justify-between items-center sticky top-0 h-[48px] border">
         <NavLogo></NavLogo>
         <div className="flex flex-row gap-2 bg-[#0a192f]">
             {
                 topics.map(
                     (topic, index) =>
-                        <NavButton key={"NavButton" + index} name={topic} ></NavButton>
+                        <NavButton key={"NavButton" + index} name={topic} href={hrefs[index]}></NavButton>
                 )
             }
         </div>
